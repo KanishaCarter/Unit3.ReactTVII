32c0a7648bbfb01da62a413b49aaa13e5e151f68
Kanisha <kanishacarter90@outlook.com>
2025-07-03 16:57:46 -0500

    .

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,12 +1,6 @@
 import "./shows.css";
 import {EpisodeList} from './episodes/EpisodeList.jsx'
 
-/*
-
- 
-with the name of the show as both the key and the text of the anchor.
-8. When a show is clicked, the selected show is updated in state to the show that was clicked.
-*/
 
 /** A navbar that allows users to choose between a list of shows */
 
@@ -14,6 +8,8 @@ with the name of the show as both the key and the text of the anchor.
 export default function ShowSelection({shows, setSelectedShow}) {
   return <nav className="shows">
     {shows.map((show) => (
+      /*the name of the show as both the key and the text of the anchor*/
+      /*8. When a show is clicked, the selected show is updated in state to the show that was clicked.*/
       <a key={show.name} class="show" onClick={()=>setSelectedShow(show)}>{show.name}</a>
     ))}
   </nav>;
